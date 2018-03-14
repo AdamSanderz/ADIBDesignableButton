7e969bc65d5413babf94ea2eb04c0d538c3c49db
Mohamad Rahmani <mohamadrahmani@acacadc1.ipt.aol.com>
2018-03-14 20:03:37 +0330

    created the ADIBDesignableButton pod with pod lib create command

---
+++ Example/Pods/Target Support Files/ADIBDesignableButton/ADIBDesignableButton-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_ADIBDesignableButton : NSObject
+@end
+@implementation PodsDummy_ADIBDesignableButton
+@end
---
+++ Example/Pods/Target Support Files/Pods-ADIBDesignableButton_Example/Pods-ADIBDesignableButton_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ADIBDesignableButton_Example : NSObject
+@end
+@implementation PodsDummy_Pods_ADIBDesignableButton_Example
+@end
